9c9207067dd303341aef146229cfaaa1e5aa39db
valonso <vmalonso@gmail.com>
2020-08-27 10:53:58 +0200

    Added exercise 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    print("Hello World.")
+    println("Hello World.")
 }
 
 // TODO: 1.- Write a function called exercise1()
@@ -7,3 +7,20 @@ fun main() {
 // println() and ivoke it from main().
 // Look for how to make a “for” loop here:
 // https://kotlinlang.org/docs/reference/control-flow.html
+
+// TODO: 2.- Uncomment this function:
+/* fun exercise2() {
+    var month = 1
+    while(month != 0) {
+        println("Enter the number of the month (0 to finish)");
+        val monthStr = readLine()
+        if (month > 0 && month <= 12) {
+            month = monthStr!!.toInt() // Don't worry about the !! yet. We will study it later
+            // TODO: 2 write the number of days that has that month, using the "when" structure
+            // You can read it about it here:
+            // https://kotlinlang.org/docs/reference/control-flow.html
+            // take into account that some months have 30, others 31 and one 28 or 29.
+            // Invoke the function from main()
+        }
+    }
+} */
