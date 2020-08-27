5a4c2ea35ddc5c55fc38ea73320210301fd25a49
valonso <vmalonso@gmail.com>
2020-08-27 10:55:27 +0200

    Fixed type

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ fun main() {
 
 // TODO: 1.- Write a function called exercise1()
 // that prints the numbers from 1 to 10 with
-// println() and ivoke it from main().
+// println() and invoke it from main().
 // Look for how to make a “for” loop here:
 // https://kotlinlang.org/docs/reference/control-flow.html
 
